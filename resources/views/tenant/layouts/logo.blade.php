a33cb517075e49e46ad7515929755a91388bd370
Alex Bethel <github@twoclicks.com.br>
2026-02-17 23:15:32 -0300

    feat: ajuste-index-people

---
+++ resources/views/tenant/layouts/logo.blade.php
@@ -0,0 +1,6 @@
+<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0 me-lg-18">
+    <a href="index.html">
+        <img alt="Logo" src="{{ asset('assets/media/logos/logo-light-2x.svg') }}" class="h-25px d-sm-none" />
+        <img alt="Logo" src="{{ asset('assets/media/logos/logo-light-2x.svg') }}" class="h-70px d-none d-sm-block" />
+    </a>
+</div>
